3aa4ef045638d521bd5259ebe4dce9ab53ec9a39
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-30 12:30:33 -0500

    fix types and technologies nullable name

--- database/migrations/2023_01_23_133158_create_types_table.php
+++ database/migrations/2023_01_23_133158_create_types_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->string('name', 50)->nullable();
             $table->string('slug', 50);
             $table->timestamps();
         });
