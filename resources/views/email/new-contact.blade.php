6a4b649bda52578f63dc6998899b886a46d48ed1
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-31 08:59:14 -0500

    create lead controller and add new-contact blade

---
+++ resources/views/email/new-contact.blade.php
@@ -0,0 +1,11 @@
+<h1>New message for Luca</h1>
+
+<p>
+    You received a new message: <br>
+
+    Name: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Object: {{ $lead->object }} <br>
+    Message: <br>
+    {{ $lead->message }}
+</p>
