08c125ce3bb0e27670038c141367f1efbf3b4f51
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-31 09:21:38 -0500

    create vue comp ContactForm

---
+++ resources/js/components/ContactForm.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+import { BASE_URL } from '../data/data'
+
+export default {
+    name: 'ContactForm',
+
+}
+</script>
+
+<template>
+    <form>
+        <div>
+            <input type="text" placeholder="Name">
+        </div>
+        <div>
+            <input type="text" placeholder="Email">
+        </div>
+        <div>
+            <input type="text" placeholder="Object">
+        </div>
+        <div>
+           <textarea name="" id="" cols="30" placeholder="Message" rows="10"></textarea>
+        </div>
+        <button>Send</button>
+    </form>
+
+</template>
+
+
+<style lang="scss" scoped>
+    div {
+        padding-bottom: 20px;
+    }
+    .is-invalid {
+        border: 1px solid red;
+    }
+    .error {
+        font-size: 0.8rem;
+        color: red;
+        margin: 0;
+    }
+
+</style>
--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -1,11 +1,16 @@
 <script>
+import ContactForm from '../components/ContactForm.vue';
 export default {
-    name: 'Contacts'
+    name: 'Contacts',
+    components: {
+        ContactForm
+    }
 }
 </script>
 
 <template>
     <h1>Contacts</h1>
+    <ContactForm />
 </template>
 
 
