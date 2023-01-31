7b647e62a603f27011ee797c071812b85d3721e9
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-31 10:08:11 -0500

    received mail into mailtrap

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
@@ -31,6 +33,8 @@ class LeadController extends Controller
         $new_lead = new Lead();
         $new_lead->fill($data);
         $new_lead->save();
+
+        Mail::to('lucafabbozzo@portfolio.com')->send(new NewContact($new_lead));
 
         return response()->json($data);
     }
